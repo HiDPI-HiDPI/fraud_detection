46bebc90332933d567e4686ee1cc09c731b82a10
HiDPI <>
2023-01-03 22:08:28 +0900

    2023/01/03 Modified log output method and SubMain to echo server

--- src/main/java/com/example/rulecheck/RuleCheckPubMain.java
+++ src/main/java/com/example/rulecheck/RuleCheckPubMain.java
@@ -1,31 +1,73 @@
 package src.main.java.com.example.rulecheck;
 
-import java.io.OutputStream;
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class RuleCheckPubMain {
-    public static void main(String[] args) {
-        System.out.println("Hello World !");
-        pubscriber();
+    private static LocalDateTime date = LocalDateTime.now();
+    private static DateTimeFormatter dataFormat = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+    private static final String bootTime = dataFormat.format(date);
+
+    private static final String PUB_LOG = "pub_log";
+    private static final String LOG_FILE_PATH = "./log/pub/";
+    private static final String LOG_FILE_NAME = "publog";
+    private static final String PUB_LOG_FILE = LOG_FILE_PATH + LOG_FILE_NAME + "_" +bootTime + ".log";
+
+    private static final String IP_ADDRESS = "192.168.0.64";
+    private static final int PORT = 7777;
+
+    public static final Logger logger = Logger.getLogger(PUB_LOG);
+
+    public static void main(String[] args) throws SecurityException, IOException {
+        Logger logger = Logger.getLogger(PUB_LOG);
+        Handler handler = new FileHandler(PUB_LOG_FILE);
+        logger.addHandler(handler);
+        Formatter formatter = new SimpleFormatter();
+        handler.setFormatter(formatter);
+        logger.log(Level.INFO, "Start PubMain");
+        //String to publish
+        String pubData = args[0];
+        logger.log(Level.INFO, "Pub:" + pubData);
+        
+        pubscriber(pubData);
     }
 
-    public static void pubscriber() {
+    public static void pubscriber(String pubData) {
+        //Simplify close processing with try-with-resources
         try (
-            SocketChannel socket = SocketChannel.open(new InetSocketAddress("192.168.0.64", 7777));){
-            System.out.println("Start!");
+            //Open SocketChannel 
+            SocketChannel socket = SocketChannel.open(new InetSocketAddress(IP_ADDRESS, PORT));){
+            logger.log(Level.INFO, "Opne SocketChannel");
 
-            String pubData = "TEST";
+            
+
+
+            
+            //Put string in ByteBuffer
             ByteBuffer bb = ByteBuffer.allocate(1024);
             ByteBuffer bb_r = bb.put(pubData.getBytes(StandardCharsets.UTF_8));
-            bb_r.flip();
-            System.out.println(bb_r);
-            socket.write(bb_r);
 
-            System.out.println("End!");
+            //Pub！
+            bb_r.flip();
+            socket.write(bb_r);
+            
+            //Return from sub
+            bb_r.clear();
+            socket.read(bb_r);
+            bb.flip();
+            logger.log(Level.INFO, "Return from sub:" + StandardCharsets.UTF_8.decode(bb).toString());
+            logger.log(Level.INFO, "End PubMain!");
              
         } catch (Exception e) {
             // TODO: handle exception
--- src/main/java/com/example/rulecheck/RuleCheckSubMain.java
+++ src/main/java/com/example/rulecheck/RuleCheckSubMain.java
@@ -1,40 +1,91 @@
 package src.main.java.com.example.rulecheck;
 
-import java.io.InputStream;
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class RuleCheckSubMain {
-    public static void main(String[] args) {
-        System.out.println("Hello World !");
+    private static LocalDateTime date = LocalDateTime.now();
+    private static DateTimeFormatter dataFormat = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+    private static final String bootTime = dataFormat.format(date);
+
+    private static final String SUB_LOG = "sub_log";
+    private static final String LOG_FILE_PATH = "./log/sub/";
+    private static final String LOG_FILE_NAME = "sublog";
+    private static final String SUB_LOG_FILE = LOG_FILE_PATH + LOG_FILE_NAME + "_" +bootTime + ".log";
+    
+    private static final int PORT = 7777;
+
+    public static final Logger logger = Logger.getLogger(SUB_LOG);
+    
+    public static void main(String[] args) throws SecurityException, IOException {
+        Logger logger = Logger.getLogger(SUB_LOG);
+        Handler handler = new FileHandler(SUB_LOG_FILE);
+        logger.addHandler(handler);
+        Formatter formatter = new SimpleFormatter();
+        handler.setFormatter(formatter);
+        logger.log(Level.INFO, "Start SubMain");
         subscriber();
     }
 
     public static void subscriber() {
+        //Simplify close processing with try-with-resources
         try (
+            //Open ServerSocketChannel and Selector
             ServerSocketChannel socket = ServerSocketChannel.open();
             Selector selector = Selector.open();){
+            logger.log(Level.INFO, "Opne ServerSocketChannel");
+            logger.log(Level.INFO, "Opne Selector");
 
-            socket.socket().bind(new InetSocketAddress(7777));
-            SocketChannel sc = socket.accept();
-            sc.configureBlocking(false);
+            socket.socket().bind(new InetSocketAddress(PORT));
+            socket.configureBlocking(false);            
+            socket.register(selector, SelectionKey.OP_ACCEPT);
+
+            while(selector.select() > 0){
+                
+                Iterator iterator = selector.selectedKeys().iterator();
+
+                while(iterator.hasNext()){
+                    SelectionKey selectionKey = (SelectionKey)iterator.next();
+                    iterator.remove();
+                    ByteBuffer bb = ByteBuffer.allocate(1024);
+
+                    if (selectionKey.isAcceptable()) {
+                        //System.out.println("isAcceptable");
+                        ServerSocketChannel ssc = (ServerSocketChannel)selectionKey.channel();
+                        SocketChannel sc = ssc.accept();
+                        sc.configureBlocking(false);
+                        sc.register(selector, SelectionKey.OP_READ);
+                        
+                    } else if (selectionKey.isReadable()) {
+                        SocketChannel sc = (SocketChannel)selectionKey.channel();
+                        //System.out.println("isReadable");
+                        selectionKey = sc.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                        bb =  ByteBuffer.class.cast( selectionKey.attachment() );
+                        if (sc.read(bb) > 0) {
+                            bb.flip();
+                            sc.write(bb);
+                            bb.rewind();
+                            logger.log(Level.INFO, StandardCharsets.UTF_8.decode(bb).toString());
+                        }
+                    }
+                }
+            }
             
-            SelectionKey key = sc.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
-            selector.select();
-            ByteBuffer bb =  ByteBuffer.class.cast( key.attachment() );
-            sc.read(bb);
-            bb.flip();
-
-            System.out.println(StandardCharsets.UTF_8.decode(bb).toString());
-
 
         } catch (Exception e) {
             // TODO: handle exception
